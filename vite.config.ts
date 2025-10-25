683a213a593513b09c9ef5c858db4080b227e9d2
yukipy <132532146+yuki010cpu@users.noreply.github.com>
2025-10-26 04:07:24 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Path dasar diatur sesuai dengan nama repositori GitHub Anda.
-  // Ini sangat penting agar GitHub Pages dapat menemukan aset Anda dengan benar.
-  base: '/warung-isekai/',
-})
+  base: '/isekaiyuki.web.id/',
