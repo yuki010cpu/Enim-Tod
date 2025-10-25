84b94325ce59096f4b20904e2275f7b542cb5a90
yukipy <132532146+yuki010cpu@users.noreply.github.com>
2025-10-26 04:14:05 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // Gunakan '/' kalau domain custom seperti isekaiyuki.web.id
+})
