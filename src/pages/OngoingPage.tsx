e5f0300282b3fd383a040c59f3da9aa36abd92a0
yukipy <132532146+yuki010cpu@users.noreply.github.com>
2026-03-25 11:44:45 +0700

    Update OngoingPage to use getTopAiringAnime from jikanService

--- src/pages/OngoingPage.tsx
+++ src/pages/OngoingPage.tsx
@@ -1,42 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import jikanService from '../services/jikanService';
 
 const OngoingPage = () => {
-    const [animeData, setAnimeData] = useState([]);
+  const [animeList, setAnimeList] = useState([]);
 
-    const fetchAnimeData = async () => {
-        try {
-            const response = await fetch('https://www.sankavollerei.com/anime/');
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-            setAnimeData(data);
-        } catch (error) {
-            console.error('Error fetching anime data:', error);
-        }
+  useEffect(() => {
+    const fetchAnime = async () => {
+      try {
+        const response = await jikanService.getTopAiringAnime();
+        setAnimeList(response);
+      } catch (error) {
+        console.error('Error fetching anime:', error);
+      }
     };
 
-    useEffect(() => {
-        fetchAnimeData();
-    }, []);
+    fetchAnime();
+  }, []);
 
-    return (
-        <div>
-            <h1>Ongoing Anime</h1>
-            <div>
-                {animeData.map(anime => (
-                    <div key={anime.id}>
-                        <h2>{anime.title}</h2>
-                        <img src={anime.thumbnail} alt={anime.title} />
-                        <div>
-                            <a href={anime.videoLink} target="_blank">Watch</a>
-                            <a href={anime.streamingLink} target="_blank">Stream</a>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Ongoing Anime</h1>
+      <ul>
+        {animeList.map(anime => (
+          <li key={anime.mal_id}>{anime.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default OngoingPage;
